0473c795a38b62b5eb6eb73314fe7b149f0428b4
Treeve <treevew@gmail.com>
2021-04-06 21:24:17 +0100

    Added Notes for Jack

--- CA3/src/socialmedia/Account.java
+++ CA3/src/socialmedia/Account.java
@@ -2,6 +2,13 @@ package socialmedia;
 
 /**
  * Account
+ * 
+ * Hey mate,
+ * we need to keep a record of all authors so maybe do like a hashset as a static variable we can just add
+ * all the handles (usernames) to? We can also then just have a arry of all users in the Social Media class.
+ * 
+ * eg 
+ * private static final Set<String> usernames = new HashSet<String>();
  */
 public class Account {
 
