f7ff222c9b46237325fcc5326e9685cb54eb941f
Treeve <treevew@gmail.com>
2021-04-21 13:19:37 +0100

    added tests

--- CA3/src/TreeveTest.java
+++ CA3/src/TreeveTest.java
@@ -29,28 +29,36 @@ public class TreeveTest {
                                             AccountIDNotRecognisedException {
 
         SocialMedia mySocialMedia = new SocialMedia();
-        
-        // Testing Account Methods
 
         int account0 = mySocialMedia.createAccount("test0");
 
-        mySocialMedia.updateAccountDescription("test8", "Added desc");
+        mySocialMedia.updateAccountDescription("test0", "Added desc");
 
-        // int account1 = mySocialMedia.createAccount("test1", "this is an account is test 1.");
+        int account1 = mySocialMedia.createAccount("test1", "this is an account is test 1.");
 
-        // int post0 = mySocialMedia.createPost("test0", "this is a post made by test1");
+        int post0 = mySocialMedia.createPost("test0", "this is a post made by test1");
         
-        // int endorsement0 = mySocialMedia.endorsePost("test0", post0);
+        int endorsement0 = mySocialMedia.endorsePost("test1", post0);
 
-        // int comment0 = mySocialMedia.commentPost("test0", post0, "this is a comment on post0 by test0");
-        // int comment1 = mySocialMedia.commentPost("test0", comment0, "this is a comment on comment0 by test0");
-        // int comment2 = mySocialMedia.commentPost("test0", post0, "this is a comment on post0 by test0");
-        // int comment3 = mySocialMedia.commentPost("test0", comment2, "this is a comment on comment2 by test0");
-
+        int comment0 = mySocialMedia.commentPost("test0", post0, "this is a comment on post0 by test0");
+        int comment1 = mySocialMedia.commentPost("test0", comment0, "this is a comment on comment0 by test0");
+        int comment2 = mySocialMedia.commentPost("test0", post0, "this is a comment on post0 by test0");
+        int comment3 = mySocialMedia.commentPost("test0", comment2, "this is a comment on comment2 by test0");
+        
+        System.out.println("Accounts:" );
         System.out.println(mySocialMedia.showAccount("test0"));
+        System.out.println(mySocialMedia.showAccount("test1"));
         
+        System.out.println("\nPosts: ");
+        System.out.println(mySocialMedia.showPostChildrenDetails(post0));
         
-
+        System.out.println("\nAnalytics: ");
+        System.out.println("most endorse account - " + mySocialMedia.getMostEndorsedAccount());
+        System.out.println("most endorse post - " + mySocialMedia.getMostEndorsedPost());
+        System.out.println("num accounts - " + mySocialMedia.getNumberOfAccounts());
+        System.out.println("num comment - " + mySocialMedia.getTotalCommentPosts());
+        System.out.println("num endorse - " + mySocialMedia.getTotalEndorsmentPosts());
+        System.out.println("num posts - " + mySocialMedia.getTotalOriginalPosts());
     }
 
 }
